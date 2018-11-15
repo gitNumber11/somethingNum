9c05ad66feb6d66cd5c7247e58f698b9cca18be8
soo <ssheln@naver.com>
2018-11-15 20:41:39 +0900

    Bootstrap resume theme

--- src/routes.js
+++
@@ -1,8 +0,0 @@
-import PositionView from './components/PositionView.vue'
-import CurrentPositions from './components/CurrentPositions.vue'
-
-export default [
-    {path: '/', component: CurrentPositions},
-    {path: '/position-view/:id', component: PositionView},
-    {path: '/current-positions', component: CurrentPositions} 
-]
